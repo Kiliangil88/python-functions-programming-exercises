5c72c74325ed08c232be839812aaad2b201ab004
Kiliangil88 <149784768+Kiliangil88@users.noreply.github.com>
2025-09-24 15:50:09 +0000

    Ejercicios resueltos

--- exercises/08-Function-parameters/app.py
+++ exercises/08-Function-parameters/app.py
@@ -1,6 +1,6 @@
 # Your code goes here:
-def render_person(param):
-    return param
+def render_person(name, cumple, eyes, edad, gender):
+    return name + ' is a ' + str(edad) + ' years old ' + gender + ' born in ' + cumple + ' with ' + eyes + ' eyes' 
 
 
 # Do not edit below this line
